48a9be21d560489f3ab8cfa9f9fff5e15f0654c3
LordBrandon <55195655+LordBrandon@users.noreply.github.com>
2019-11-21 09:13:21 -0500

    Love - ACTUAL Final Product
    This is the final product!

---
+++ Love - Website Final Product/pr1.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+function confirmInfo() {
+  alert("Transaction Complete! Thank you " + document.forum.name.value + ", for donating a value of " + document.forum.amount.value + ". We appreciate this donation and will use this to make students' education much better.");
+
+window.location = '../';
+}
